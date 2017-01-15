7e259e9719fd882fc48f99cb39ad4e3af7d547cc
Antonio Lopes <aclopesjr@hotmail.com>
2017-01-15 17:48:37 -0200

    Final Capstone Project

--- Capstone/FacialTracker/FacialTracker/ViewController.swift
+++ Capstone/FacialTracker/FacialTracker/ViewController.swift
@@ -13,13 +13,31 @@ class ViewController: UIViewController, UICollectionViewDataSource, FaceTrackerV
 
     // MARK: Local Variables
     weak var faceTrackerViewController: FaceTrackerViewController?
-    var currentImageView = UIImageView()
-    var currentImageIndex: Int = -1
+    
+    var currentMoustacheImageView = UIImageView()
+    var currentMoustacheImageIndex: Int = -1
+    var moustacheImageCollection: [UIImage] = []
+    
+    var currentHatImageView = UIImageView()
+    var currentHatImageIndex: Int = -1
+    var hatImageCollection: [UIImage] = []
+    
     var overlayViews = [UIView]()
-    var imageCollection: [UIImage] = []
     
     @IBOutlet var imagePickerView: UIView!
     @IBOutlet weak var imageCollectionView: UICollectionView!
+    
+    var isRotated = false
+    @IBOutlet var mainButton: UIButton!
+    @IBOutlet var moustacheButton: UIButton!
+    @IBOutlet var glassesButton: UIButton!
+    @IBOutlet var hatButton: UIButton!
+    
+    var featureImage: FeatureImage!
+    enum FeatureImage {
+        case Moustache
+        case Hat
+    }
     
     // MARK: Others
     
@@ -27,11 +45,16 @@ class ViewController: UIViewController, UICollectionViewDataSource, FaceTrackerV
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        self.view.addSubview(currentImageView)
-        currentImageView.contentMode = UIViewContentMode.ScaleAspectFit
+        self.view.addSubview(self.currentMoustacheImageView)
+        self.currentMoustacheImageView.contentMode = UIViewContentMode.ScaleAspectFit
+        
+        self.view.addSubview(self.currentHatImageView)
+        self.currentHatImageView.contentMode = UIViewContentMode.ScaleAspectFit
         
         self.fillImageCollection()
-        openImagePickerView()
+        
+        self.moustacheButton.alpha = 0
+        self.hatButton.alpha = 0
     }
 
     override func didReceiveMemoryWarning() {
@@ -55,29 +78,46 @@ class ViewController: UIViewController, UICollectionViewDataSource, FaceTrackerV
     // MARK: CollectionView
     
     func fillImageCollection() {
-        self.imageCollection.append(UIImage(named: "moustache01")!)
-        self.imageCollection.append(UIImage(named: "moustache02")!)
-        self.imageCollection.append(UIImage(named: "moustache03")!)
-        self.imageCollection.append(UIImage(named: "hat01")!)
-        self.imageCollection.append(UIImage(named: "hat02")!)
-        self.imageCollection.append(UIImage(named: "hat03")!)
+        self.moustacheImageCollection.removeAll()
+        self.moustacheImageCollection.append(UIImage(named: "moustache01")!)
+        self.moustacheImageCollection.append(UIImage(named: "moustache02")!)
+        self.moustacheImageCollection.append(UIImage(named: "moustache03")!)
+        self.moustacheImageCollection.append(UIImage(named: "cancel")!)
+        
+        self.hatImageCollection.removeAll()
+        self.hatImageCollection.append(UIImage(named: "hat01")!)
+        self.hatImageCollection.append(UIImage(named: "hat02")!)
+        self.hatImageCollection.append(UIImage(named: "hat03")!)
+        self.hatImageCollection.append(UIImage(named: "cancel")!)
+    }
+    
+    func setImageCollection(featureImage: FeatureImage) {
+        self.featureImage = featureImage
         
         //Display
         self.imageCollectionView.reloadData()
     }
     
-//    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-//        return self.imageCollection.count
-//    }
-    
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageCollection.count
+        switch self.featureImage! {
+        case FeatureImage.Moustache:
+            return self.moustacheImageCollection.count
+        default:
+            return self.hatImageCollection.count
+        }
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell{
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Images", forIndexPath: indexPath) as! ImageCollectionViewCell
         
-        cell.ImagePreview.image = self.imageCollection[indexPath.row]
+        switch self.featureImage! {
+        case FeatureImage.Moustache:
+            cell.ImagePreview.image = self.moustacheImageCollection[indexPath.row]
+            break
+        default:
+            cell.ImagePreview.image = self.hatImageCollection[indexPath.row]
+            break
+        }
         
         cell.clipsToBounds = true
         cell.layer.borderWidth = 1
@@ -91,7 +131,13 @@ class ViewController: UIViewController, UICollectionViewDataSource, FaceTrackerV
     
     //Cell touched action
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        self.currentImageIndex = indexPath.row
+        
+        switch self.featureImage! {
+        case FeatureImage.Moustache:
+            self.currentMoustacheImageIndex = indexPath.row
+        default:
+            self.currentHatImageIndex = indexPath.row
+        }
         
         UIView.animateWithDuration(0.4, animations: {
             self.imagePickerView.alpha = 0
@@ -101,9 +147,41 @@ class ViewController: UIViewController, UICollectionViewDataSource, FaceTrackerV
             }
         }
     }
-
+    
     // MARK: Button area
-    @IBAction func onClickImageSelector(sender: AnyObject) {
+    @IBAction func onClickMainButton(sender: UIButton) {
+        animateFloatingButtons()
+    }
+    
+    func animateFloatingButtons() {
+        if (isRotated) {
+            UIView.animateWithDuration(0.5) { () -> Void in
+                self.mainButton.transform = CGAffineTransformMakeRotation(0)
+            }
+            UIView.animateWithDuration(0.25, delay: 0.25, options: UIViewAnimationOptions.AllowAnimatedContent, animations: {
+                self.moustacheButton.alpha = 0
+                self.hatButton.alpha = 0
+                self.isRotated = false
+                }, completion: nil)
+        } else {
+            UIView.animateWithDuration(0.5) { () -> Void in
+                self.mainButton.transform = CGAffineTransformMakeRotation(CGFloat(M_PI * 0.25))
+            }
+            UIView.animateWithDuration(0.25, delay: 0.25, options: UIViewAnimationOptions.AllowAnimatedContent, animations: {
+                self.moustacheButton.alpha = 0.5
+                self.hatButton.alpha = 0.5
+                self.isRotated = true
+                }, completion: nil)
+        }
+    }
+    
+    @IBAction func onClickMoustacheButton() {
+        setImageCollection(FeatureImage.Moustache)
+        openImagePickerView()
+    }
+    
+    @IBAction func onClickHatButton() {
+        setImageCollection(FeatureImage.Hat)
         openImagePickerView()
     }
     
@@ -114,9 +192,11 @@ class ViewController: UIViewController, UICollectionViewDataSource, FaceTrackerV
         self.view.addSubview(self.imagePickerView)
         self.view.bringSubviewToFront(self.imagePickerView)
         
-        UIView.animateWithDuration(0.4, animations: {
-            self.imagePickerView.alpha = 1
-        })
+        UIView.animateWithDuration(0.4, delay: 0, options: UIViewAnimationOptions.AllowAnimatedContent, animations: {
+                self.imagePickerView.alpha = 1
+            }) { (Bool) in
+                self.animateFloatingButtons()
+        }
     }
     
     // MARK: Face Tracker
@@ -148,18 +228,26 @@ class ViewController: UIViewController, UICollectionViewDataSource, FaceTrackerV
 //            for (index, point) in points.innerMouth.enumerate() {
 //                updateViewForFeature(index, point: point, bgColor: UIColor.redColor())
 //            }
+//            
+//            for (index, point) in points.outerMouth.enumerate() {
+//                updateViewForFeature(index, point: point, bgColor: UIColor.yellowColor())
+//            }
 //        }
         
-        if self.currentImageIndex > -1  {
-            self.currentImageView.image = self.imageCollection[self.currentImageIndex]
+        self.currentMoustacheImageView.image = nil
+        if (self.currentMoustacheImageIndex > -1 && self.currentMoustacheImageIndex < self.moustacheImageCollection.count - 1) {
+            self.currentMoustacheImageView.image = self.moustacheImageCollection[self.currentMoustacheImageIndex]
+            
+            // Tracker for moustache
+            moustacheFaceTracker(points)
+        }
+        
+        self.currentHatImageView.image = nil
+        if (self.currentHatImageIndex > -1 && self.currentHatImageIndex < self.hatImageCollection.count - 1) {
+            self.currentHatImageView.image = self.hatImageCollection[self.currentHatImageIndex]
             
             // Tracker for hats
-            if self.currentImageIndex > 2 {
-                hatFaceTracker(points)
-            // Tracker for moustache
-            } else {
-                moustacheFaceTracker(points)
-            }
+            hatFaceTracker(points)
         }
     }
     
@@ -185,43 +273,54 @@ class ViewController: UIViewController, UICollectionViewDataSource, FaceTrackerV
         
         if let points = points {
             // Compute the frame
-            let innerMouthCornerDist = sqrt(pow(points.innerMouth[0].x - points.innerMouth[4].x, 2))
-            let innerMouthCenter = CGPointMake((points.innerMouth[0].x + points.innerMouth[4].x) / 2, (points.innerMouth[0].y + points.innerMouth[4].y) / 2)
+            let innerMouthCornerDist = sqrt(pow(points.innerMouth[0].x - points.innerMouth[4].x, 2)) * 2.0
+            
+            let innerMouthCenter = CGPointMake(
+                points.outerMouth[3].x,
+                (points.innerMouth[0].y + points.innerMouth[4].y) / 2
+            )
             
             let angle = atan2(points.rightEye[5].y - points.leftEye[0].y, points.rightEye[5].x - points.leftEye[0].x)
             
-            updateFaceTracker(points, cornerDist: innerMouthCornerDist, center: innerMouthCenter, angle: angle)
+            updateFaceTracker(self.currentMoustacheImageView, points: points, cornerDist: innerMouthCornerDist, center: innerMouthCenter, angle: angle)
             
         } else {
-            currentImageView.hidden = true
+            self.currentMoustacheImageView.hidden = true;
         }
     }
     
     func hatFaceTracker(points: FacePoints?) {
         if let points = points {
             // Compute the hat frame
-            let eyeCornerDist = sqrt(pow(points.leftEye[0].x - points.rightEye[5].x, 2) + pow(points.leftEye[0].y - points.rightEye[5].y, 2))
-            let eyeToEyeCenter = CGPointMake((points.leftEye[0].x + points.rightEye[5].x) / 2, (points.leftEye[0].y + points.rightEye[5].y) / 2)
+            let eyeCornerDist = sqrt(pow(points.leftEye[0].x - points.rightEye[5].x, 2)) * 1.5
             
-            let angle = atan2(points.rightEye[5].y - points.leftEye[0].y, points.rightEye[5].x - points.leftEye[0].x)
+            let eyeToEyeCenter = CGPointMake(
+                (points.leftEye[0].x + points.rightEye[5].x) / 2,
+                (points.leftEye[0].y + points.rightEye[5].y) / 2
+            )
             
-            updateFaceTracker(points, cornerDist: eyeCornerDist, center: eyeToEyeCenter, angle: angle)
+            let angle = atan2(
+                points.rightEye[5].y - points.leftEye[0].y,
+                points.rightEye[5].x - points.leftEye[0].x
+            )
+            
+            updateFaceTracker(self.currentHatImageView, points: points, cornerDist: eyeCornerDist, center: eyeToEyeCenter, angle: angle)
         } else {
-            currentImageView.hidden = true
+            self.currentHatImageView.hidden = true
         }
     }
     
-    func updateFaceTracker(points: FacePoints?, cornerDist: CGFloat, center: CGPoint, angle: CGFloat) {
-        let viewWidth = 2.0 * cornerDist
-        let viewHeight = (currentImageView.image!.size.height / currentImageView.image!.size.width) * viewWidth
+    func updateFaceTracker(imageView: UIImageView, points: FacePoints?, cornerDist: CGFloat, center: CGPoint, angle: CGFloat) {
+        let viewWidth = cornerDist
+        let viewHeight = (imageView.image!.size.height / imageView.image!.size.width) * viewWidth
             
-        currentImageView.transform = CGAffineTransformIdentity
-        currentImageView.frame = CGRectMake(center.x - viewWidth / 2, center.y - 1.3 * viewHeight, viewWidth, viewHeight)
-        currentImageView.hidden = false
+        imageView.transform = CGAffineTransformIdentity
+        imageView.frame = CGRectMake(center.x - viewWidth / 2, center.y - 1.3 * viewHeight, viewWidth, viewHeight)
+        imageView.hidden = false
             
-        setAnchorPoint(CGPointMake(0.5, 1.0), forView: currentImageView)
+        setAnchorPoint(CGPointMake(0.5, 1.0), forView: imageView)
             
-        currentImageView.transform = CGAffineTransformMakeRotation(angle)
+        imageView.transform = CGAffineTransformMakeRotation(angle)
     }
 }
 
